3e1f2cc168e80b36bb84882628593d314a374143
bbiijjeett <bijeetnathbtech@gmail.com>
2024-10-28 11:52:29 +0530

    service update 1

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,40 +7,40 @@ import { IoIosMail } from "react-icons/io";
 
 const services = [
   {
-    "title": "Brand Identity",
-    "description": "A service focused on creating and developing a unique brand identity that reflects the company's values and differentiates it in the market."
+    "title": "Branding and Social media",
+    "description": "A service centered on establishing a cohesive brand presence across social media platforms, aligning with the company's values while engaging and expanding its audience."
   },
   {
     "title": "Website Design & Development",
-    "description": "A service that offers professional website design and development, creating user-friendly and visually appealing websites to enhance online presence."
+    "description": "A service dedicated to designing and building dynamic websites that not only showcase the brand’s identity but also provide a seamless user experience, driving engagement and conversions."
   },
   {
     "title": "Graphic Design & Illustrations",
-    "description": "A service that provides creative graphic design and illustrations, including logos, marketing materials, and custom artwork, to enhance brand visuals."
+    "description": "A service focused on creating captivating graphic designs and illustrations that communicate the brand's message effectively and enhance its visual storytelling."
   }
 ]
 
 const socialMediaPackages = [
   {
-    "name": "12 Social Media Creatives/m",
+    "name": "12 Custom Social Media Creatives/Month",
   },
   {
-    "name": "Custom Strategy"
+    "name": "Tailored Social Media Strategy"
   },
   {
-    "name": "Dedicated SM Manager"
+    "name": "Dedicated Social Media Manager"
   },
   {
-    "name": "Dedicated Copywriter"
+    "name": "Professional Copywriting Services"
   },
   {
-    "name": "Unlimited Revisions"
+    "name": "Unlimited Design Revisions"
   },
   {
-    "name": "Pause Anytime"
+    "name": "Flexible Pause Option"
   },
   {
-    "name": "Performance Report/m",
+    "name": "Monthly Performance Reports",
   }
 ];
 
@@ -49,46 +49,46 @@ const litePackage = [
     "name": "2 Packs of Your Choice"
   },
   {
-    "name": "48hrs Turn Around Time"
+    "name": "48-Hour Turnaround Time"
   },
   {
-    "name": "Dedicated Team"
+    "name": "Dedicated Project Team"
   },
   {
-    "name": "Pause Anytime"
+    "name": "Flexible Pause Option"
   },
   {
-    "name": "Single Milestone/48hrs",
+    "name": "Single Milestone Delivery",
   },
   {
     "name": "Post-Delivery Support"
   },
   {
-    "name": "Performance Report/m",
+    "name": "Monthly Performance Reports",
   }
 ];
 
 const customPackage = [
   {
-    "name": "Bespoke project proposal"
+    "name": "Tailored Project Proposal"
   },
   {
-    "name": "Create your own Pack"
+    "name": "Create Your Own Package"
   },
   {
-    "name": "Custom Project Timeline"
+    "name": "Customizable Project Timeline"
   },
   {
-    "name": "Delivered in Milestones"
+    "name": "Milestone-Based Delivery"
   },
   {
-    "name": "Fixed scope of work"
+    "name": "Defined Scope of Work"
   },
   {
-    "name": "Weekly Design Sync"
+    "name": "Weekly Design Sync Meetings"
   },
   {
-    "name": "Performance Report/m",
+    "name": "Monthly Performance Reports",
   },
   {
     "name": "Post-Delivery Support"
@@ -131,10 +131,10 @@ const Services = () => {
       <div></div>
       <div className='bg-[#FAF7F0] h-fit w-full rounded-3xl p-5'>
         <div className='flex flex-col md:flex-row p-5 '>
-          <div className='md:w-1/2 p-10'>
-            <p className='font-light text-sm mb-2'>Branding and Graphic design</p>
-            <h1 className='font-semibold text-lg mb-2'>Visual Communication and Strategic Branding</h1>
-            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>We transform brands through visual storytelling and meaningful design. Combining strategic branding with compelling creative, we enable brands to communicate powerfully and authentically.</p>
+          <div className='md:w-1/2 py-5 sm:p-10'>
+            <p className='font-light text-sm mb-2'>Branding and Social media</p>
+            <h1 className='font-semibold text-lg mb-2'>Building Identities and Digital Connections</h1>
+            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>Helping brands define their identity and connect with audiences through impactful branding and engaging social media strategies.</p>
             <button className='bg-red-500 text-white px-4 py-2 rounded-full font-semibold text-center'>Start Today</button>
           </div>
           <div className=' md:w-1/2  overflow-hidden'>
@@ -145,28 +145,28 @@ const Services = () => {
           <div className='md:w-1/2  overflow-hidden'>
             <img className='aspect-video h-full w-full rounded-3xl' src='https://framerusercontent.com/images/feZKpDJv0jnJKp1Xwh5sy8SxSY.gif' />
           </div>
-          <div className='md:w-1/2 p-10'>
-            <p className='font-light text-sm mb-2'>Branding and Graphic design</p>
-            <h1 className='font-semibold text-lg mb-2'>Visual Communication and Strategic Branding</h1>
-            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>We transform brands through visual storytelling and meaningful design. Combining strategic branding with compelling creative, we enable brands to communicate powerfully and authentically.</p>
+          <div className='md:w-1/2 py-5 sm:p-10'>
+            <p className='font-light text-sm mb-2'>Website Design & Development</p>
+            <h1 className='font-semibold text-lg mb-2'>Crafting Engaging Digital Experiences</h1>
+            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>Building responsive, user-centered websites that blend design and functionality to elevate your online presence.</p>
             <button className='bg-red-500 text-white px-4 py-2 rounded-full font-semibold text-center'>Start Today</button>
           </div>
         </div>
         <div className='flex flex-col md:flex-row p-5'>
-          <div className='md:w-1/2 p-10'>
-            <p className='font-light text-sm mb-2'>Branding and Graphic design</p>
-            <h1 className='font-semibold text-lg mb-2'>Visual Communication and Strategic Branding</h1>
-            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>We transform brands through visual storytelling and meaningful design. Combining strategic branding with compelling creative, we enable brands to communicate powerfully and authentically.</p>
+          <div className='md:w-1/2 py-5 sm:p-10'>
+            <p className='font-light text-sm mb-2'>Graphic Design & Illustrations</p>
+            <h1 className='font-semibold text-lg mb-2'>Bringing Ideas to Life with Artful Precision and Creativity</h1>
+            <p className='font-normal text-base lg:w-2/3 text-gray-500 mb-5'>We create visually stunning designs that not only capture attention but also effectively convey your brand's message.</p>
             <button className='bg-red-500 text-white px-4 py-2 rounded-full font-semibold text-center'>Start Today</button>
           </div>
           <div className='md:w-1/2  overflow-hidden'>
             <img className='aspect-video h-full w-full rounded-3xl' src='https://framerusercontent.com/images/Mh7W5IkUcHeA1Usj0CnzNmuJvns.gif' />
           </div>
         </div>
-        <div className='p-10 flex flex-col md:flex-row gap-5'>
+        <div className='p-2 md:p-10 flex flex-col md:flex-row gap-5'>
           <div className='w-full md:w-1/2'>
-            <h1 className='font-bold text-6xl mb-5'>Packs</h1>
-            <p className='font-light text-2xl md:w-2/3'>We are experts in crafting bold visuals, distinctive brand identities, innovative products, and immersive websites.</p>
+            <h1 className='font-bold text-5xl md:text-6xl mb-5'>Our Bundles</h1>
+            <p className='font-light text-2xl md:w-2/3 text-pretty'>We specialize in creating striking visuals, unique brand identities, cutting-edge products, and engaging websites, all driven by a team of talented young professionals.</p>
           </div>
           <div className=' w-full md:w-1/2'>
             <Accordion items={services} />
@@ -236,15 +236,15 @@ const Services = () => {
           </div>
         </div>
       </div> */}
-      <div className='flex flex-col h-auto gap-5 my-10 mx-10'>
-        <h1 className='font-bold text-5xl md:text-7xl text-white text-center'>Pricing</h1>
+      <div className='flex flex-col h-auto gap-5 my-10 mx-2 md:mx-10'>
+        <h1 className='font-bold text-5xl md:text-7xl text-white text-center mb-2'>Pricing</h1>
         <div className='flex flex-col lg:flex-row h-full px-2 md:px-5 lg:px-20 justify-center items-center gap-5'>
           
           {/* Social Media Package */}
-          <div className='bg-white h-full w-full lg:w-1/3 rounded-xl py-5'>
+          <div className='bg-white h-full sm:min-h-[100vh] w-full lg:w-1/3 rounded-xl py-5'>
             <div className='flex flex-col p-5 md:p-10'>
               <p className='font-light text-base md:text-lg capitalize'>Social media package</p>
-              <h1 className='font-bold text-4xl md:text-5xl'>$397/m</h1>
+              <h1 className='font-bold text-4xl md:text-5xl'>$399/m</h1>
               <p className='font-normal text-lg md:text-xl'>Perfect to get started with kyte</p>
             </div>
             <div className='flex flex-col justify-center items-center px-5 md:px-10 mb-5'>
@@ -252,18 +252,21 @@ const Services = () => {
             </div>
             <div className='flex flex-col justify-center items-center px-5 md:px-10 gap-2'>
               {socialMediaPackages.map((item, index) => (
-                <div key={index} className='bg-[#EFEFEF] w-full text-black px-4 py-2 rounded-full font-semibold text-center'>
-                  <p className='font-light text-lg flex place-items-center'><MdRadioButtonChecked size={24} style={{ marginRight: '5px' }} />{item.name}</p>
+                <div key={index} className="bg-[#EFEFEF] w-full text-black px-4 py-2 rounded-full font-semibold ">
+                  <p className="font-light text-lg flex items-center">
+                    <MdRadioButtonChecked size={24} style={{ marginRight: '5px' }} />
+                    {item.name}
+                  </p>
                 </div>
               ))}
             </div>
           </div>
 
           {/* Lite Package */}
-          <div className='bg-white h-full w-full lg:w-1/3 rounded-xl py-5'>
+          <div className='bg-white h-full sm:min-h-[100vh] w-full lg:w-1/3 rounded-xl py-5'>
             <div className='flex flex-col p-5 md:p-10'>
               <p className='font-light text-base md:text-lg capitalize'>Lite Package</p>
-              <h1 className='font-bold text-4xl md:text-5xl'>$1997/m</h1>
+              <h1 className='font-bold text-4xl md:text-5xl'>$699/m</h1>
               <p className='font-normal text-lg md:text-xl'>For Small And Midsize Business</p>
             </div>
             <div className='flex flex-col justify-center items-center px-5 md:px-10 mb-5'>
@@ -279,7 +282,7 @@ const Services = () => {
           </div>
 
           {/* Single Project */}
-          <div className='bg-white h-full w-full lg:w-1/3 rounded-xl py-5   '>
+          <div className='bg-white h-full sm:min-h-[100vh] w-full lg:w-1/3 rounded-xl py-5   '>
             <div className='flex flex-col p-5 md:p-10'>
               <p className='font-light text-base md:text-lg capitalize'>Single project</p>
               <h1 className='font-bold text-4xl md:text-5xl'>Custom</h1>
@@ -302,7 +305,7 @@ const Services = () => {
       </div>
 
 
-      <div className='flex flex-col gap-5 my-5 lg:my-10 h-fit'>
+      {/* <div className='flex flex-col gap-5 my-5 lg:my-10 h-fit'>
         <h1 className='font-bold text-7xl text-white text-center'>Still Unsure?</h1>
         <div className='flex flex-col md:flex-row justify-center items-center gap-5 px-10 lg:px-20'>
           <div className='w-full md:w-1/2 h-full  bg-white rounded-2xl p-10'>
@@ -324,10 +327,12 @@ const Services = () => {
             <CustomButton  title={'Share Your Questions'} />
           </div>
         </div>
-      </div>
+      </div> */}
       <Flywithus/>
     </section>
   )
 }
 
 export default Services
+
+
